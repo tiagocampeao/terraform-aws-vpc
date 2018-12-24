e1cd5a7e057c5b37f5643c310cf75d48b140b85d
tiagocampeao <tiagocampeao@outlook.com>
2018-12-24 20:40:51 -0200

    terceiro commit

--- main.tf
+++ main.tf
@@ -1,9 +1,24 @@
-resource "aws_vpc" "main" {
- cidr_block       = "10.0.0.0/16"
- instance_tenancy = "default"
-
+provider "aws" {
+  region     = "${var.region}"
+}
+resource "aws_vpc" "vpc" {
+ cidr_block       = "${var.vpc_cidr}"
+ #instance_tenancy = "default"
+ 
  tags {
    Name = "main"
  }
 }
 
+resource "aws_subnet" "subnet" {
+	count	=	"3"
+	count 			  = "${length(data.aws_availability_zones.azs.names)}"
+	availability_zone =	"${element(data.aws_availability_zones.azs.names,count.index)}"
+	vpc_id			  =	"${aws_vpc.vpc.id}"
+	cidr_block 		  = "${element(var.subnet_cidr,count.index)}"
+
+	tags {
+		Name = "subnet${count.index+1}"
+	}
+}
+	
--- variables.tf
+++ variables.tf
@@ -3,6 +3,12 @@ variable "aws" {
 	default = ""
 }
 
+variable "region" {
+	description	=	"armazena região"
+	default = "us-east-1"
+}
+
+
 variable "aws_vpc" {
 	description	=	"cria vpc"
 	default	=	""
@@ -13,7 +19,32 @@ variable "instance_tenancy"	{
 	default	=	""
 }
 
+variable "vpc_cidr" {
+	description	=	"define range ip da vpc"
+	default	=	"10.0.0.0/16"
+}
+
+variable "subnet_cidr" {
+	description	=	"define range ip da subnet"
+	type	=	"list"
+	default	=	["10.0.1.0/24","10.0.2.0/24","10.0.3.0/24"]
+}
+
 variable "cidr_block" {
 	description = "blocos de subrede da vpc"
 	default = ""
 }
+
+#variable "aws_subnet" {
+#	description	=	"monta a subnet na vpc"
+#	default	=	""
+#}
+
+#variable "azs" {
+#	description	=	"define zona de disponibilidade por subnet"
+#	type	=	"list"
+#	default	=	["us-east-1a,us-east-1b,us-east-1c"]
+#}
+
+data "aws_availability_zones" "azs"	{}
+
